c76b6414608168dbd20d24bacb2175c6669edb90
Martin Sebastijan Šestak <msestak@irb.hr>
2016-10-31 09:13:18 +0100

    Add initial minil skeleton files

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+PsiBlastHelper - It's new $module
+
+# SYNOPSIS
+
+    use PsiBlastHelper;
+
+# DESCRIPTION
+
+PsiBlastHelper is ...
+
+# LICENSE
+
+Copyright (C) mocnii.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+mocnii <msestak@irb.hr>
---
+++ lib/PsiBlastHelper.pm
@@ -0,0 +1,39 @@
+package PsiBlastHelper;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+PsiBlastHelper - It's new $module
+
+=head1 SYNOPSIS
+
+    use PsiBlastHelper;
+
+=head1 DESCRIPTION
+
+PsiBlastHelper is ...
+
+=head1 LICENSE
+
+Copyright (C) Martin Sebastijan Šestak.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+mocnii E<lt>msestak@irb.hrE<gt>
+
+=cut
+
